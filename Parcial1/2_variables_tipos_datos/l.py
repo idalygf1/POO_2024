abbe2901cbca86c43c3c3408d5da80c334ce9db6
idalygf1 <garciaidaly386@gmail.com>
2024-05-18 01:51:13 -0600

    si

---
+++ Parcial1/2_variables_tipos_datos/l.py
@@ -0,0 +1,6 @@
+#Crear un programa que permita calcular e imprimir el precio a pagar por un articulo.
+#En el precio a pagar se incluye el IVA. El programa deberá de funcionar n veces como el usuario desee 
+
+IVA=0.16
+
+precio=input("Ingrese el pre
