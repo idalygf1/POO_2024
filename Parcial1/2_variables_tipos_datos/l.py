76579c7ec7adac581d428adc883a8d5944cf1d21
idalygf1 <145083756+idalygf1@users.noreply.github.com>
2024-05-19 22:00:17 -0600

    SI

--- Parcial1/2_variables_tipos_datos/l.py
+++ Parcial1/2_variables_tipos_datos/l.py
@@ -1,7 +1,11 @@
 #Crear un programa que permita calcular e imprimir el precio a pagar por un articulo.
-#En el precio a pagar se incluye el IVA. El programa deberá de funcionar n veces como el usuario desee 
+#En el precio a pagar se incluye el IVA. El programa debera de funcionar n veces como el usuario desee 
 
 IVA=0.16
 
+precio=input("Ingrese el precio a pagar: ")
+precio_neto=(precio * IVA)
 
-print=()
+precio_pagar=precio + precio_neto
+
+print=("El precio a pagar es de: ")
