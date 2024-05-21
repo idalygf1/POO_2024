6112197e3e5c049235ee0eb856a7d50f10032dfb
idalygf1 <145083756+idalygf1@users.noreply.github.com>
2024-05-21 10:59:06 -0600

    si

--- Parcial1/6_Ejercicios_repaso_1al5/Ejercicio_6.py
+++ Parcial1/6_Ejercicios_repaso_1al5/Ejercicio_6.py
@@ -1,13 +1,74 @@
 #Mostrar todas las tablas del 1 al 10. Mostrando el titulo de la tabla y luego las multiplicaciones del 1 al 10
 
-tabla=int(input("Ingresa un numero para calcular su tabla de multiplicar: "))
 
-
-
+num1=1
+print("Tabla de multiplicar del: {}".format(num1))
 multi=0
-
-print(f"Tabla de multiplicar {tabla}")
 for i in range(1,11):
-    multi=i*tabla
-    
-    print(f"{tabla} X {i} = {multi}")
+    multi=i*num1
+    print(f"{num1} X {i} = {multi}")
+
+
+num2=2
+print("\nTabla de multiplicar del: {}".format(num2))
+multi=0
+for i in range(1,11):
+    multi=i*num2
+    print(f"{num2} X {i} = {multi}")
+
+num3=3
+print("\nTabla de multiplicar del: {}".format(num3))
+multi=0
+for i in range(1,11):
+    multi=i*num3
+    print(f"{num3} X {i} = {multi}")
+
+
+num4=4
+print("\nTabla de multiplicar del: {}".format(num4))
+multi=0
+for i in range(1,11):
+    multi=i*num4
+    print(f"{num4} X {i} = {multi}")
+
+num5=5
+print("\nTabla de multiplicar del: {}".format(num5))
+multi=0
+for i in range(1,11):
+    multi=i*num5
+    print(f"{num5} X {i} = {multi}")
+
+num6=6
+print("\nTabla de multiplicar del: {}".format(num6))
+multi=0
+for i in range(1,11):
+    multi=i*num6
+    print(f"{num6} X {i} = {multi}")
+
+num7=7
+print("\nTabla de multiplicar del: {}".format(num7))
+multi=0
+for i in range(1,11):
+    multi=i*num7
+    print(f"{num7} X {i} = {multi}")
+
+num8=8
+print("\nTabla de multiplicar del: {}".format(num8))
+multi=0
+for i in range(1,11):
+    multi=i*num8
+    print(f"{num8} X {i} = {multi}")
+
+num9=9
+print("\nTabla de multiplicar del: {}".format(num9))
+multi=0
+for i in range(1,11):
+    multi=i*num9
+    print(f"{num9} X {i} = {multi}")
+
+num10=10
+print("\nTabla de multiplicar del: {}".format(num10))
+multi=0
+for i in range(1,11):
+    multi=i*num10
+    print(f"{num10} X {i} = {multi}")
---
+++ Parcial1/6_Ejercicios_repaso_1al5/Ejercicio_7.py
@@ -0,0 +1,5 @@
+#Hacer un programa que muestre todos los numeros impares entre 2 numeros que decida el usuario
+
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
