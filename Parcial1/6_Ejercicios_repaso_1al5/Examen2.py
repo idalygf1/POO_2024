d472f2bb089f3efd56980af9367df874b66dcd8e
idalygf1 <145083756+idalygf1@users.noreply.github.com>
2024-06-04 23:36:38 -0600

    si

---
+++ Parcial1/6_Ejercicios_repaso_1al5/Examen2.py
@@ -0,0 +1,20 @@
+#Examen 2
+basico= 0.987
+Intermedido=1.203
+IVA= .16
+impuesto= 12.56
+
+kWh=float(input("Ingrese la cantidad de kWh"))
+
+if kWh <=150:
+    tarifa= kWh - 150 
+    tarifa2= tarifa * basico
+    tarifa3= tarifa + 150
+    tarifa4 = tarifa * Intermedido
+
+
+iva = tarifa4 * IVA 
+Total_pagar = iva + impuesto
+
+
+print("Su total a pagar de recibo de luz es: {}".format(Total_pagar))
