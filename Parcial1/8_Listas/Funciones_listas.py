7ec8c88baeade04249bb02a3727ff1e8f36cdea5
idalygf1 <145083756+idalygf1@users.noreply.github.com>
2024-06-07 11:31:03 -0600

    si

--- Parcial1/8_Listas/Funciones_listas.py
+++ Parcial1/8_Listas/Funciones_listas.py
@@ -4,7 +4,7 @@ varios=["UTD",True,100,0.100]
 
 #Ordernar las listas
 print(paises)
-paises.sort()
+paises.sort()#ordenar
 print(paises)
 
 print(numero)
@@ -13,14 +13,33 @@ print(numero)
 
 #Agregar elementos a la lista
 print(numero)
-numero.append(100)
+numero.append(100)#agregar
 print(numero)
 numero.insert(len(numero),200)
 print(numero)
 
 #Remover elementos
 print(numero)
-numero.remove(100)
+numero.remove(100)#remover
 print(numero)
-numero.pop(2)
+numero.pop(2)#lo recorre
 print(numero)
+
+#Dar la vuelta a los elementos de una lista 
+print(varios)
+varios.reverse()
+print(varios)
+
+#Buscar un valor dentro de una lista
+encontro="BRASIL" in paises
+print(encontro)
+
+#Vaciar una lista 
+print(paises)
+paises.clear()
+print(paises)
+
+#Unir o concatenar listas
+print(varios)
+varios.extend(numero)
+print(varios)
