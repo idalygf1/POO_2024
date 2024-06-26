ad9c6baace0f870fed415d7b76226af5bfb92fa0
idalygf1 <145083756+idalygf1@users.noreply.github.com>
2024-06-26 09:38:35 -0600

    ER Parcial2 POO of VSCode

---
+++ Parcial2/5_encapsulamiento/main.py
@@ -0,0 +1,23 @@
+
+from coches import *
+
+class Coches:
+    print("Objeto 1")
+coche1=Coches("Blanco","VW","2022",220,150,5)
+
+
+# coche1.getInfo()
+
+
+
+
+# print("Objeto 2")
+# coche2=Coches("Azul","Nissan","2020",180,150,6)
+# coche2.getInfo()
+
+#implementar el encapsulamiento o visibilidad 
+
+print(coche1.atributo_publico)
+print(coche1.MetodoPublico())
+
+coche1.getMetodoPublico()
